5f17079880054f399a5d61b7007441e0ec0d1eb1
Suraj <suraj.sowkar@gmail.com>
2020-09-03 11:32:04 -0400

    updated region

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_dHOkborws', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5m30fc6gphgk4cabje3bi3qhh7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
