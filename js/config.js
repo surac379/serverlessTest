636ed7bf7ced68bf0cee104514bfe59c7328fb35
Suraj <suraj.sowkar@gmail.com>
2020-09-03 11:26:59 -0400

    updated title

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_dHOkborws', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5m30fc6gphgk4cabje3bi3qhh7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
